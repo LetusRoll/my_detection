8af10ff88a2683a1147fcdea633d3ca8561244af
cxy329 <1019172082@qq.com>
2021-10-23 21:15:00 +0800

    first commit

--- src/my_detection/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cxy/lidar_detection_projects/my_detection
-# Build directory: /home/cxy/lidar_detection_projects/my_detection/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
